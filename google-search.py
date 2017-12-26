698d103d05f6e2ad0e21726fd0d31c09f47280d8
Jed Helmers <jedhelmers@gmail.com>
2017-12-26 11:05:53 -0500

    Should probably open the typo file here then call spellcheck directly in the write to file

--- google-search.py
+++ google-search.py
@@ -5,18 +5,18 @@ try:
 except ImportError:
     print("No module named 'google' found")
 
+# Open typo repository
 
 
 # Search: Dump what you would say in a normal google search
 query = 'site:https://github.com AND url:README.md'
 
-for j in search(query, tld="co.in", num=10, lang='en', start=201, stop=300, pause=0):
+for j in search(query, tld="co.in", num=10, lang='en', start=401, stop=500, pause=0):
     try:
         spellcheck.spellCheck(j)
     except:
         print ""
     #print(j)
-
 #for url in urls:
 #    spellcheck.spellCheck(url)
     #print(url)
