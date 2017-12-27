c43a25a8c4cba8083ef59195da58524b4949c718
Jed Helmers <jedhelmers@gmail.com>
2017-12-26 20:05:19 -0500

    General cleanup

--- google-search.py
+++ google-search.py
@@ -11,7 +11,7 @@ except ImportError:
 # Search: Dump what you would say in a normal google search
 query = 'site:https://github.com AND url:README.md'
 
-for j in search(query, tld="co.in", num=10, lang='en', start=401, stop=500, pause=0):
+for j in search(query, tld="co.in", num=10, lang='en', start=501, stop=700, pause=0):
     try:
         spellcheck.spellCheck(j)
     except:
--- spellcheck.py
+++ spellcheck.py
@@ -24,29 +24,36 @@ def spellCheck(url):
     # Get meaningful text within Class .markdown-body
     bodyCopies = soup.find_all(class_ = 'markdown-body')
 
-    # Output header
-    print 'Site: %s' % url
-    print "Mispelled words: "
-    f.write('%s \n Mispelled words: \n' % url)
+
     # Iterate through text
     for body in bodyCopies:
         # Tag list to look through
         # Check paragraph tags. This should ignore all code-based text on the page
-        texts = body.find_all(['p', 'h1', 'h2', 'h3', 'h4', 'li'])
+        texts = body.find_all(['p', 'h1', 'h2', 'h3', 'h4'])
         for text in texts:
             p = text.getText().upper()
             chkr.set_text(p)
             try:
                 for err in chkr:
-                    # Cleanse duplicates
-                    errorList.append(err.word)
-                    for error in errorList:
-                        if err.word.upper() != error:
-                            f.write('%s \n' % err.word)
-                            print err.word
-                            break
+                    errorList.append(err.word.upper())
             except:
                 print ""
+
+
+    errorList.sort()
+    error_last = ''
+    cnt = 0
+    for error in errorList:
+        if error != error_last:
+            cnt += 1
+            if cnt == 1:
+                # Output header
+                print 'Site: %s' % url
+                print "Mispelled words: "
+                f.write('%s\nMispelled words: \n' % url)
+            f.write('%s \n' % error.lower())
+            print error.lower()
+        error_last = error
     print '\n'
     f.write('\n')
     f.close()
